17ff9e5ddbd4a24613e670980f10534a522e8431
Stridsvagn69420 <Stridsvagn69420@users.noreply.github.com>
2022-07-29 22:02:03 +0200

    Complete build script with clean command

--- build.go
+++ build.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
 )
 
 // Build Locations
@@ -11,7 +15,6 @@ type BuildLocation string
 const (
 	ZigLocation  BuildLocation = "zig-out/bin/%s"
 	RustLocation BuildLocation = "target/release/%s"
-	CLocation    BuildLocation = "%s"
 )
 
 // Build Types
@@ -23,14 +26,88 @@ const (
 	CType    BuildType = "c"
 )
 
-// Build Commands
-type BuildCmd string
+// ----------------------------------------------------------------------------------------------------------------------- //
+// ----------------------------------------------------------------------------------------------------------------------- //
+// ----------------------------------------------------------------------------------------------------------------------- //
 
-const (
-	ZigBuild  BuildCmd = "zig build"
-	RustBuild BuildCmd = "cargo zigbuild --release"
-	CBuild    BuildCmd = "zig cc main.c -o %s -s"
-)
+// Init processes
+func new_cmd(dir string, name string, args ...string) error {
+	fmt.Println(fmt.Sprintf("Building \u001b[36m%s\u001b[0m...", dir))
+	cmd := exec.Command(name, args...)
+	cmd.Dir = filepath.Join(cwd, dir)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
+
+// Build Wrapper
+func build(name string, protype BuildType) error {
+	switch protype {
+	case ZigType:
+		return new_cmd(name, "zig", "build")
+	case RustType:
+		return new_cmd(name, "cargo", "zigbuild", "--release")
+	case CType:
+		if runtime.GOOS == "windows" {
+			return new_cmd(name, "zig", "cc", "main.c", "-o", fmt.Sprintf("../build/%s.exe", name), "-s")
+		} else {
+			return new_cmd(name, "zig", "cc", "main.c", "-o", fmt.Sprintf("../build/%s", name), "-s")
+		}
+	}
+	return nil
+}
+
+// Wrapper for Build Wrapper
+func build_init() {
+	unsuccessful := 0
+	os.Mkdir(builddir, 0755)
+	for program, project := range projects {
+		err := build(program, project)
+		if err != nil {
+			unsuccessful++
+			println(err.Error())
+		} else {
+			if project == ZigType {
+				os.Rename(fmt.Sprintf(string(ZigLocation), program), filepath.Join(builddir, program))
+			}
+			if project == RustType {
+				os.Rename(fmt.Sprintf(string(RustLocation), program), filepath.Join(builddir, program))
+			}
+		}
+	}
+	if unsuccessful > 0 {
+		println(fmt.Sprintf("\u001b[31m%d programs failed to build!\u001b[0m", unsuccessful))
+		os.Exit(1)
+	} else {
+		fmt.Println("\u001b[32mAll programs built successfully!\u001b[0m")
+	}
+}
+
+// ----------------------------------------------------------------------------------------------------------------------- //
+// ----------------------------------------------------------------------------------------------------------------------- //
+// ----------------------------------------------------------------------------------------------------------------------- //
+
+// Clean Wrapper
+func clean(name string, protype BuildType) {
+	switch protype {
+	case ZigType:
+		fmt.Println("Removing \u001b[36" + name + "/zig-out/\u001b[0m...")
+		os.RemoveAll(filepath.Join(cwd, name, "zig-out"))
+		fmt.Println("Removing \u001b[36" + name + "/zig-cache/\u001b[0m...")
+		os.RemoveAll(filepath.Join(cwd, name, "zig-cache"))
+	case RustType:
+		fmt.Println("Removing \u001b[36" + name + "/target/\u001b[0m...")
+		os.RemoveAll(filepath.Join(cwd, name, "target"))
+	}
+}
+
+// ----------------------------------------------------------------------------------------------------------------------- //
+// ----------------------------------------------------------------------------------------------------------------------- //
+// ----------------------------------------------------------------------------------------------------------------------- //
+
+// Paths
+var cwd string
+var builddir string
 
 // Map of Coreutils Programs (Key: program name; Value: project type)
 var projects = map[string]BuildType{
@@ -38,31 +115,30 @@ var projects = map[string]BuildType{
 	"false": CType,
 }
 
-// Build Wrapper
-func build(name string, protype BuildType) {
-	// Run build commands
-}
-
-// Clean Wrapper
-func clean(name string, protype BuildType) {
-	// Remove build files
-}
-
 // Main Function
 func main() {
-	for program, project := range projects {
-		if len(os.Args) > 1 {
-			switch os.Args[1] {
-			case "build":
-				build(program, project)
-			case "clean":
-				clean(program, project)
-			default:
-				println(fmt.Sprintf("Unknown command: %s", os.Args[1]))
-				os.Exit(1)
-			}
-		} else {
-			build(program, project)
-		}
+	path, err := os.Getwd()
+	if err != nil {
+		log.Println(err)
 	}
+	cwd = path
+	builddir = filepath.Join(cwd, "build")
+	if len(os.Args) > 1 {
+		switch os.Args[1] {
+		case "build":
+			build_init()
+		case "clean":
+			fmt.Println("Removing \u001b[36mbuild/\u001b[0m...")
+			os.RemoveAll(builddir)
+			for program, project := range projects {
+				clean(program, project)
+			}
+		default:
+			println(fmt.Sprintf("\u001b[31mUnknown command: %s\u001b[0m", os.Args[1]))
+			os.Exit(1)
+		}
+	} else {
+		build_init()
+	}
+	os.Exit(0)
 }
